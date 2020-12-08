25f604fe6767897565ee9be7a5e08866cc09b127
Minh Bui <64919587+mithu170900@users.noreply.github.com>
2020-12-07 21:39:14 -0600

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -72,10 +72,10 @@ const siteConfig = {
     If you want to remove a social media icon, you can add the two // before the line
     or delete the line. */
     socialMedia: {
-        linkedin: 'https://www.linkedin.com/in/',
-        twitter: 'https://twitter.com/',
-        github: 'https://github.com/',
-        //facebook: 'https://www.facebook.com/',
+        linkedin: 'https://www.linkedin.com/in/minh-thu-bui-6648a4177/',
+        github: 'https://github.com/mithu170900',
+        facebook: 'https://www.facebook.com/minhthu.bui.5220/',
+        //twitter: 'https://twitter.com/',
         //instagram: 'https://www.instagram.com/',
         //youtube: 'https://www.youtube.com/',
         //twitch: 'https://www.twitch.tv/'
